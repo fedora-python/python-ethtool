acbda6affd699229f411716455704b40bbf18718
Miro Hrončok <miro@hroncok.cz>
2017-03-24 21:25:57 +0100

    Bring some CPython 2 helper code so we can run this

--- tests/test_ethtool.py
+++ tests/test_ethtool.py
@@ -21,7 +21,6 @@
 
 import sys
 import unittest
-from test.test_support import run_unittest # requires python-test subpackage on Fedora/RHEL
 
 import ethtool
 
@@ -237,6 +236,43 @@ class EthtoolTests(unittest.TestCase):
         eis = ethtool.get_interfaces_info(devnames)
         for ei in eis:
             self._verify_etherinfo_object(ei)
+
+
+def _run_suite(suite):
+    """Run tests from a unittest.TestSuite-derived class."""
+    if verbose:
+        runner = unittest.TextTestRunner(sys.stdout, verbosity=2)
+    else:
+        runner = BasicTestRunner()
+
+    result = runner.run(suite)
+    if not result.wasSuccessful():
+        if len(result.errors) == 1 and not result.failures:
+            err = result.errors[0][1]
+        elif len(result.failures) == 1 and not result.errors:
+            err = result.failures[0][1]
+        else:
+            err = "multiple errors occurred"
+            if not verbose:
+                err += "; run in verbose mode for details"
+        raise TestFailed(err)
+
+
+def run_unittest(*classes):
+    """Run tests from unittest.TestCase-derived classes."""
+    valid_types = (unittest.TestSuite, unittest.TestCase)
+    suite = unittest.TestSuite()
+    for cls in classes:
+        if isinstance(cls, str):
+            if cls in sys.modules:
+                suite.addTest(unittest.findTestCases(sys.modules[cls]))
+            else:
+                raise ValueError("str arguments must be keys in sys.modules")
+        elif isinstance(cls, valid_types):
+            suite.addTest(cls)
+        else:
+            suite.addTest(unittest.makeSuite(cls))
+    _run_suite(suite)
 
 
 if __name__ == '__main__':
